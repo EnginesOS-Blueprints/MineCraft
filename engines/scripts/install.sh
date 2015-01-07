f2541d2491032b88f4dcc88e1882e06ccf851a44
James Vodanovich <james@jvodan.com>
2015-01-07 11:56:51 +1100

    ini

--- engines/scripts/install.sh
+++ engines/scripts/install.sh
@@ -1,4 +1,4 @@
 mv /usr/local/etc/etc/mono /usr/local/etc/;
 cd /home/app;
-/home/app/MCMA2_Linux_x86_64 -setpass $Admin_Password;
+/home/app/MCMA2_Linux_x86_64 -nonotice -setpass $Admin_Password;
 
