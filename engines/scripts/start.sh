819b3360ec24bcc8c9fb96636310d081a9031860
James Vodanovich <james@jvodan.com>
2015-01-22 14:15:48 +1100

    first commit

--- engines/scripts/start.sh
+++ engines/scripts/start.sh
@@ -1,2 +1,8 @@
 cd /home/app;
+if test ! -f /home/app/Minecraft/installed
+then
+/home/app/MCMA2_Linux_x86_64 -nonotice -setpass $Admin_Password;
+touch /home/app/Minecraft/installed
+ else
 mono McMyAdmin.exe;
+fi
