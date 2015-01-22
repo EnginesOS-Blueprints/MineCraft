f90de711bf74e00f1b8e8a5524d3c1d4a3f00bf1
James Vodanovich <james@jvodan.com>
2015-01-22 14:35:36 +1100

    first commit

--- engines/scripts/start.sh
+++ engines/scripts/start.sh
@@ -1,6 +1,7 @@
 cd /home/app;
 if test ! -f /home/app/Minecraft/installed
 then
+mv /usr/local/etc/etc/mono /usr/local/etc/;
 /home/app/MCMA2_Linux_x86_64 -nonotice -setpass $Admin_Password;
 touch /home/app/Minecraft/installed
  else
